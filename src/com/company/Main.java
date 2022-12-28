e33ee0c3e80aa814298cd76e5dfd4cf736458ddd
Shandilya-saloni <114082157+Shandilya-saloni@users.noreply.github.com>
2022-12-28 15:51:36 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        //alt+ctrl+l
+        /* used for multiline comments
+
+         */
+        // int age = 5;
+        // int b = 9;
+        // int c = a + b;
+        // System.out.println("Sum of two numbers is ");
+        // System.out.println(c);
+        //agr print hi likha ln use nhi kia to next line pr nhi aayega output
+        //System.out.println("Sum of two numbers is " + c );   here + operator is used for pasting (jese c me format specifier % use hota tha wesa yaha java me + use hoga)
+        // sum of 5 and 9 is
+    }
+}
